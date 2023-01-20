142d526cda54e4c381e2be870345757f6a0e6d8e
Dovran <d.ovetdiyev@gmail.com>
2023-01-20 23:44:40 +0500

    props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import './assets/index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Booklist() {
+  return (
+    
+    <section className='booklist'>
+      
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+
+    </section>
+  );
+  
+
+}
+const Book=()=>{
+  return (
+    <article>
+      <Image/>
+      <Title/>
+      <Author/>
+
+    </article>
+  )
+};
+const Image=()=><img src="https://cdn.dsmcdn.com/ty324/product/media/images/20220206/6/45542573/380901818/1/1_org.jpg" alt="" /> 
+
+
+const Author=()=><h4>ROBERT WALDINGER</h4>;
+const Title=()=><h5>Lessons from world's best writer</h5>;
+
+ReactDOM.render(<Booklist/>, document.getElementById('root')); 
+  
